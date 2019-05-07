d6cd81fb529a6fb923bc0802f94f308079b4f623
Princeling <2230647190@qq.com>
2019-05-07 13:58:49 +0800

    准备编写闹钟

---
+++ Alarm_clock.srcs/sources_1/new/alarm_clock_top.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2019/05/07 13:57:09
+// Design Name: 
+// Module Name: alarm_clock_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alarm_clock_top(
+    input   wire        sys_clk,
+    input   wire        rst_n,
+    output  
+    );
+
+
+
+endmodule
